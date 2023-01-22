3bcc420f8db29f72ad6a7a7b866ba7b8789b4869
Otya063 <airdisaster063@gmail.com>
2023-01-22 17:32:06 +0900

    updated bottom_navigation

--- frontend/src/lib/BottomMenu.svelte
+++ frontend/src/lib/BottomMenu.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    import { HtmlTag } from "svelte/internal";
     import { cached_lang } from "../lang/i18n";
     import { jaTrans, enTrans } from "../lang/translation";
 
     let language: string;
     cached_lang.subscribe((e) => (language = e));
     $: translator = () => (language == "ja" ? jaTrans.bottom_menu : enTrans.bottom_menu);
+
+    const openMenu = () => {
+        const html = document.documentElement;
+        const bottom_menu = document.querySelector(".side_menu");
+        bottom_menu.classList.toggle("open");
+        html.classList.toggle("fixed");
+    };
 </script>
 
 <ul>
@@ -14,7 +22,7 @@
     </li>
 
     <!-- アイコン2つ目-->
-    <li class="openbtn">
+    <li class="openbtn" on:click={openMenu}>
         <span class="mini-text">{translator().lang_sel}</span>
     </li>
 
