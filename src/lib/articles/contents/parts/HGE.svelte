447ed1591dc03367c002f3683a8b50aa358f66c0
Otya063 <airdisaster063@gmail.com>
2023-03-29 00:54:10 +0900

    Add table_data style and some tweaks

--- src/lib/articles/contents/parts/HGE.svelte
+++ src/lib/articles/contents/parts/HGE.svelte
@@ -1,75 +1,66 @@
 <tr class="table_row">
     <th class="table_header" scope="row" />
-    <td class="table_data_center no_border_right table_header_hge">High Grade Edition 動作環境</td>
+    <td style="border-left: black solid 2px" class="table_data table_data_center no_border_right table_header_hge">High Grade Edition 動作環境</td>
 </tr>
 <tr class="table_row">
-    <th class="table_header" scope="row">
-        目安となる<br />
-        解像度
-    </th>
-    <td class="table_data_center no_border_right">1920&times;1080</td>
+    <th class="table_header" scope="row">解像度</th>
+    <td class="table_data table_data_center no_border_right">1920×1080</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">OS</th>
-    <td class="no_border_right" colspan="2">
-        <p>
-            Windows 7, 8, 8.1, 10 (32bit/64bit※ 日本語版)<br />
-            ※64bit版はWOW64 (Windows on Windows 64) での動作です。<br />
-            ※OSによって必要な動作環境が別にございますので、ご確認ください。<br />
-            ※Windows は米国 Microsoft Corporation の米国およびその他の国における登録商標です。
-        </p>
+    <td class="table_data no_border_right" colspan="2">
+        <ul>
+            <li>Windows 7, 8, 8.1, 10（32bit/64bit 日本語版）</li>
+            <li style="color: red;">※64bit版はWOW64（Windows on Windows 64）での動作です。</li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">CPU</th>
-    <td class="table_data_center no_border_right">
-        <p style="margin-bottom: 5px; line-height: 1.1em">インテル&reg;　Core&trade; i5 2.66GHz以上</p>
-    </td>
+    <td class="table_data table_data_center no_border_right">インテル® Core™ i5 2.66GHz以上</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">メモリ</th>
-    <td class="table_data_center no_border_right">4GB以上</td>
+    <td class="table_data table_data_center no_border_right">4GB以上</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">
-        グラフィックス<br />
-        ボード
+        グラフィック<br />
+        カード
     </th>
-    <td class="table_data_center no_border_right" style="font-size: 89%">
-        NVIDIA&reg; Geforce&reg; GTX 660 (2GB) シリーズ以上<br />
-        または同クラスのグラフィックスボード以上
+    <td class="table_data table_data_center no_border_right">
+        <ul>
+            <li>NVIDIA® Geforce® GTX 660（2GB）シリーズ以上</li>
+            <li>または同クラスのグラフィックスボード以上</li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
-    <th class="table_header" scope="row">DirectX&reg;</th>
-    <td colspan="2" class="table_data_center no_border_right">
-        DirectX&reg; 9.0c以上<br />
-        ※DirectXが既にインストールされているパソコンでも、DirectX エンド ユーザー ランタイムが別途必要となります。<br />
-        DirectX エンド ユーザー ランタイムのダウンロードは<a
-            href="https://web.archive.org/web/20191218011610/https://www.microsoft.com/ja-jp/download/details.aspx?id=34429"
-            target="_blank"
-            rel="noreferrer">こちら</a
-        >
+    <th class="table_header" scope="row">DirectX®</th>
+    <td colspan="2" class="table_data no_border_right">
+        <ul style="padding-left: 1em;">
+            <li style="text-align: center; padding-right: 1em">DirectX® 9.0c以上</li>
+            <li style="text-indent: -1em;">※DirectXが既にインストールされているパソコンでも、DirectX エンド ユーザー ランタイムが別途必要となります。</li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">サウンド</th>
-    <td colspan="2" class="table_data_center no_border_right">
-        DirectSound&reg;対応16bitステレオ44.1kHz<br />
-        または48kHz WAVE形式が再生可能なサウンドカード
+    <td colspan="2" class="table_data table_data_center no_border_right">
+        <ul>
+            <li>DirectSound®対応16bitステレオ44.1kHz</li>
+            <li>または48kHz WAVE形式が再生可能なサウンドカード</li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
-    <th class="table_header" scope="row">
-        ハードディスク<br />
-        空き容量
-    </th>
-    <td class="table_data_center no_border_right" colspan="2">20GB以上必須</td>
+    <th class="table_header" scope="row">HDD空き容量</th>
+    <td class="table_data table_data_center no_border_right" colspan="2">20GB以上必須</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">
         インターネット<br />
         接続環境
     </th>
-    <td style="border-bottom:0px" class="table_data_center no_border_right" colspan="2">ADSL1.0Mbps以上の常時接続インターネット回線</td>
+    <td class="table_data table_data_center no_border_right" colspan="2">ADSL1.0Mbps以上の常時接続インターネット回線</td>
 </tr>
--- src/lib/articles/contents/parts/Original.svelte
+++ src/lib/articles/contents/parts/Original.svelte
@@ -1,95 +1,103 @@
 <tr class="table_row">
     <th class="table_header" scope="row" />
-    <td class="table_data_center table_header_original">推奨動作環境</td>
-    <td class="table_data_center no_border_right table_header_original">必要動作環境</td>
+    <td style="border-left: black solid 2px" class="table_data table_data_center table_header_original">推奨動作環境</td>
+    <td class="table_data table_data_center no_border_right table_header_original">必要動作環境</td>
 </tr>
 <tr class="table_row">
-    <th class="table_header" scope="row">
-        目安となる<br />
-        解像度
-    </th>
-    <td class="table_data_center">1600&times;1200</td>
-    <td class="table_data_center no_border_right">800x600</td>
+    <th class="table_header" scope="row">解像度</th>
+    <td class="table_data table_data_center">1600×1200</td>
+    <td class="table_data table_data_center no_border_right">800×600</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">OS</th>
-    <td class="no_border_right" colspan="2">
-        <p>
-            Windows 7, 8, 8.1, 10 (32bit/64bit※ 日本語版)<br />
-            ※64bit版はWOW64 (Windows on Windows 64) での動作です。<br />
-            ※OSによって必要な動作環境が別にございますので、ご確認ください。
-        </p>
+    <td class="table_data no_border_right" colspan="2">
+        <ul>
+            <li>Windows 7, 8, 8.1, 10（32bit/64bit 日本語版）</li>
+            <li style="color: red;">※64bit版はWOW64（Windows on Windows 64）での動作です。</li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
     <th scope="row" class="table_header" rowspan="2">CPU</th>
-    <td>
-        <p style="margin-bottom: 5px; line-height: 1.1em">インテル&reg; Core&trade;2（Duo、Extreme、Quad）シリーズ</p>
-        <p style="margin-bottom: 5px; line-height: 1.1em">インテル&reg; Core&trade; (i3、i5、i7)シリーズ</p>
-        <p style="margin-bottom: 5px; line-height: 1.1em">インテル&reg; Core&trade; (i5-2000、i7-2000) シリーズ ※1</p>
-        <p style="margin-bottom: 5px; line-height: 1.1em">AMD Athlon&trade; X2 デュアルコア・プロセッサ4600+以上</p>
-        <p style="margin-bottom: 5px; line-height: 1.1em">AMD Phenom&trade; シリーズ</p>
-        <p style="margin-bottom: 5px; line-height: 1.1em">AMD AthlonII&trade; シリーズ</p>
-        <p style="margin-bottom: 5px; line-height: 1.1em">AMD PhenomII&trade; シリーズ</p>
-        <p style="margin-bottom: 0; line-height: 1.1em">AMD A-Series APU(A8-3850,A6-3650) ※2</p>
+    <td class="table_data">
+        <ul class="table_data_list">
+            <li class="table_data_list_item">インテル® Core™2（Duo, Extreme, Quad）シリーズ<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">インテル® Core™（i3, i5, i7）シリーズ<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">インテル® Core™（i5-2000, i7-2000）シリーズ<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Athlon™ X2 デュアルコア・プロセッサ4600+以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Phenom™ シリーズ<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD AthlonII™ シリーズ<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD PhenomII™ シリーズ<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD A-Series APU（A8-3850, A6-3650） ※2<span class="table_data_list_item_part" /></li>
+        </ul>
     </td>
-    <td class="no_border_right">
-        <p style="margin-bottom: 5px; line-height: 1.1em">インテル&reg;　Pentium&reg; 4 2.0GHz以上</p>
-        <p style="margin-bottom: 0; line-height: 1.1em">AMD Athlon&trade;64 3500+以上</p>
+    <td class="table_data no_border_right">
+        <ul class="table_data_list">
+            <li class="table_data_list_item">インテル® Pentium® 4 2.0GHz以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Athlon™64 3500+以上<span class="table_data_list_item_part" /></li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
-    <td colspan="2" class="table_data_center no_border_right">SSE対応のCPU</td>
+    <td colspan="2" class="table_data table_data_center no_border_right">SSE対応のCPU</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">メモリ</th>
-    <td class="table_data_center">1GB以上</td>
-    <td class="table_data_center no_border_right">512MB以上</td>
+    <td class="table_data table_data_center">1GB以上</td>
+    <td class="table_data table_data_center no_border_right">512MB以上</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">
-        グラフィックス<br />
-        ボード
+        グラフィック<br />
+        カード
     </th>
-    <td style="font-size: 89%">
-        NVIDIA&reg; GeForce&reg; 8600シリーズ以上<br />
-        NVIDIA&reg; GeForce&reg; 9600シリーズ以上<br />
-        NVIDIA&reg; GeForce&reg; 200シリーズ以上<br />
-        AMD Radeon&trade; HD 2600シリーズ以上<br />
-        AMD Radeon&trade; HD 3650シリーズ以上<br />
-        AMD Radeon&trade; HD 4650シリーズ以上<br />
-        AMD Radeon&trade; HD 5550シリーズ以上<br />
-        AMD Radeon&trade; HD 6450シリーズ以上
+    <td class="table_data">
+        <ul class="table_data_list">
+            <li class="table_data_list_item">NVIDIA® GeForce® 8600シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">NVIDIA® GeForce® 9600シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">NVIDIA® GeForce® 200シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 2600シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 3650シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 4650シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 5550シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 6450シリーズ以上<span class="table_data_list_item_part" /></li>
+        </ul>
     </td>
-    <td class="no_border_right" style="font-size: 89%">
-        NVIDIA&reg; GeForce&reg; FX　5700シリーズ以上<br />
-        NVIDIA&reg; GeForce&reg; 6600シリーズ以上<br />
-        NVIDIA&reg; GeForce&reg; 7600シリーズ以上<br />
-        ATi Radeon&trade; 9500シリーズ以上<br />
-        AMD Radeon&trade; HD 4350シリーズ以上<br />
-        AMD Radeon&trade; HD 5450シリーズ以上
+    <td class="table_data no_border_right">
+        <ul class="table_data_list">
+            <li class="table_data_list_item">NVIDIA® GeForce® FX 5700シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">NVIDIA® GeForce® 6600シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">NVIDIA® GeForce® 7600シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">ATi Radeon™ 9500シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 4350シリーズ以上<span class="table_data_list_item_part" /></li>
+            <li class="table_data_list_item">AMD Radeon™ HD 5450シリーズ以上<span class="table_data_list_item_part" /></li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
-    <th class="table_header" scope="row">DirectX&reg;</th>
-    <td colspan="2" class="table_data_center no_border_right">DirectX 9.0c以上</td>
-</tr>
-<tr class="table_row">
-    <th class="table_header" scope="row">サウンド</th>
-    <td colspan="2" class="table_data_center no_border_right">
-        DirectSound&reg;対応16bitステレオ44.1kHz<br />
-        または48kHz WAVE形式が再生可能なサウンドカード
-    </td>
+    <th class="table_header" scope="row">DirectX®</th>
+    <td colspan="2" class="table_data table_data_center no_border_right">DirectX 9.0c以上</td>
 </tr>
 <tr class="table_row">
     <th class="table_header" scope="row">
-        ハードディスク<br />
-        空き容量
-    </th>
-    <td class="no_border_right" colspan="2">
-        インストールするハードディスク内に20GB以上必須 <br />
-        ※上記の空き容量にはDirectX&reg; インストール用の領域、OSのスワップファイル領域を含みます。<br />
-        ※インストーラが一時的に使用する作業領域を含みます。この作業領域はルートドライブ(通常はC:ドライブ) に必要で、インストール後に開放されます。
+        サウンド<br />
+        カード</th
+    >
+    <td colspan="2" class="table_data table_data_center no_border_right">
+        <ul>
+            <li>DirectSound®対応16bitステレオ44.1kHz</li>
+            <li>または48kHz WAVE形式が再生可能なサウンドカード</li>
+        </ul>
+    </td>
+</tr>
+<tr class="table_row">
+    <th class="table_header" scope="row">HDD空き容量</th>
+    <td class="table_data no_border_right" colspan="2">
+        <ul style="padding-left: 1em;">
+            <li style="text-indent: -1em;">インストールするハードディスク内に20GB以上必須</li>
+            <li style="text-indent: -1em;">※上記の空き容量にはDirectX® インストール用の領域、OSのスワップファイル領域を含みます。</li>
+            <li style="text-indent: -1em;">※インストーラーが一時的に使用する作業領域を含みます。この作業領域はルートドライブ（通常はC:ドライブ） に必要で、インストール後に開放されます。</li>
+        </ul>
     </td>
 </tr>
 <tr class="table_row">
@@ -97,5 +105,5 @@
         インターネット<br />
         接続環境
     </th>
-    <td style="border-bottom:0px" class="table_data_center no_border_right" colspan="2">ADSL1.0Mbps以上の常時接続インターネット回線</td>
+    <td class="table_data table_data_center no_border_right" colspan="2">ADSL1.0Mbps以上の常時接続インターネット回線</td>
 </tr>
